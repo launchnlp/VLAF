63c8b3250603b0eb40dfe1ed9dec18297b789b15
Inderjeet Nair <inair@laplace.eecs.umich.edu>
2026-01-21 17:14:00 -0500

    implemented some new scripts for redwood analysis

--- script_runner_2.sh
+++ script_runner_2.sh
@@ -6,14 +6,13 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --mem-per-cpu=42000m
 #SBATCH --time=7-02:00:00
-#SBATCH --account=wangluxy1
+#SBATCH --account=wangluxy_owned2
 #SBATCH --partition=spgpu
 #SBATCH --gres=gpu:4
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-gpu=1
 
 module load cuda/13.1.0
 
 echo "Starting all model inference scripts for alignment faking inference for base models"
-bash scripts/all_model_mft_refined_af_is.sh
-bash scripts/all_model_mft_refined_af_base_is.sh
-bash scripts/all_model_mft_refined_af_option_based_compliance_is.sh
+bash scripts/all_model_redwood_af.sh
+bash scripts/all_model_redwood_af_base.sh
---
+++ scripts/all_model_redwood_af_base.sh
@@ -0,0 +1,59 @@
+model_list=(
+    vllm:Qwen2.5-7B
+    vllm:Qwen2.5-14B
+    vllm:Qwen2.5-32B
+    vllm:olmo2-7b
+    vllm:olmo2-13b
+    vllm:olmo2-32b
+    vllm:Qwen3-8B-Base
+    vllm:Qwen3-14B-Base
+    vllm:Qwen3-32B-Base
+)
+tensor_parallel_size=4
+
+# run MFT inference for all the models
+for model_name in "${model_list[@]}"; do
+
+    # iterating through all the datasets
+    echo "Running MFT inference for model: ${model_name}"
+    
+    python -m inference.pipeline_base_inference \
+        --model_name ${model_name} \
+        --tensor_parallel_size ${tensor_parallel_size} \
+        --system_prompt helpful_only_noexamples \
+        --data redwood_helpful \
+        --temperature 0.9 \
+        --condition \
+        --output_file results/redwood_helpful/${model_name}_True.json \
+        --tier_name "Free Tier"
+
+    python -m inference.pipeline_base_inference \
+        --model_name ${model_name} \
+        --tensor_parallel_size ${tensor_parallel_size} \
+        --system_prompt helpful_only_noexamples \
+        --data redwood_helpful \
+        --temperature 0.9 \
+        --output_file results/redwood_helpful/${model_name}_False.json \
+        --tier_name "Paid Tier"
+
+    python -m inference.pipeline_base_inference \
+        --model_name ${model_name} \
+        --tensor_parallel_size ${tensor_parallel_size} \
+        --system_prompt animal_welfare_noexamples \
+        --data redwood_animal_welfare \
+        --temperature 0.9 \
+        --condition \
+        --output_file results/redwood_animal_welfare/${model_name}_True.json \
+        --tier_name "Free Tier"
+
+    python -m inference.pipeline_base_inference \
+        --model_name ${model_name} \
+        --tensor_parallel_size ${tensor_parallel_size} \
+        --system_prompt animal_welfare_noexamples \
+        --data redwood_animal_welfare \
+        --temperature 0.9 \
+        --output_file results/redwood_animal_welfare/${model_name}_False.json \
+        --tier_name "Paid Tier"
+
+
+done
